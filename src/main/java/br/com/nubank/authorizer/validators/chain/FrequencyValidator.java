6f36f65ec18f5cbe71d91d06440f268c42acf5f6
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-09-12 22:47:55 -0300

    refactor: remove unnecessary check for current transaction

--- src/main/java/br/com/nubank/authorizer/validators/chain/FrequencyValidator.java
+++ src/main/java/br/com/nubank/authorizer/validators/chain/FrequencyValidator.java
@@ -9,26 +9,30 @@ import java.util.List;
 public class FrequencyValidator extends BaseHandler {
 
     private static final String HIGH_FREQUENCY_VIOLATION = "high-frequency-small-interval";
+    private static final int FREQUENCY_MINUTES_RANGE = 2;
+    private static final int MAXIMUM_TRANSACTIONS_ALLOWED = 3;
+    private static final int CURRENT_TRANSACTION_COUNT = 1;
 
     @Override
     public void handle(Operation operation, List<String> violations) {
         List<Transaction> transactionsHistory = operation.getTransactionsHistory();
 
-        LocalDateTime start = LocalDateTime.now().minusMinutes(2);
+        LocalDateTime start = LocalDateTime.now().minusMinutes(FREQUENCY_MINUTES_RANGE);
         LocalDateTime end = LocalDateTime.now();
 
-        long transactionsInLastTwoMinutesFromHistory = transactionsHistory.stream()
-                .filter(transaction -> isInvalidRange(start, end, transaction.getTime()))
-                .count();
+        long transactionsInLastTwoMinutesFromHistory = getLastTwoMinutesTransactionsCount(transactionsHistory, start, end);
 
-        LocalDateTime currentTransactionTime = operation.getCurrentTransaction().getTime();
-        int currentTransaction = isInvalidRange(start, end, currentTransactionTime) ? 1 : 0;
-
-        if (transactionsInLastTwoMinutesFromHistory + currentTransaction > 3) {
+        if (transactionsInLastTwoMinutesFromHistory + CURRENT_TRANSACTION_COUNT > MAXIMUM_TRANSACTIONS_ALLOWED) {
             violations.add(HIGH_FREQUENCY_VIOLATION);
         }
 
         super.handle(operation, violations);
+    }
+
+    private long getLastTwoMinutesTransactionsCount(List<Transaction> transactionsHistory, LocalDateTime start, LocalDateTime end) {
+        return transactionsHistory.stream()
+                .filter(transaction -> isInvalidRange(start, end, transaction.getTime()))
+                .count();
     }
 
     private boolean isInvalidRange(LocalDateTime start, LocalDateTime end, LocalDateTime time) {
